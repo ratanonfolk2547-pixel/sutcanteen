38381564b8d2e71716f9a4d016019376c6d017a5
folk <ratanonfolk2547@gmail.com>
2026-05-29 15:54:19 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.static("public"));
+
+const PORT = 3000;
+
+const SHEET_ID =
+"1mGWbCXw20W81Xf3exwGvJ7bU6jpU-3NYLziMZ3kPGgA";
+
+const API_KEY =
+"AIzaSyCbgwSn_yJ8Y-z2fP528bF_5GjbTlYyUP4";
+
+const RANGE =
+encodeURIComponent("การตอบแบบฟอร์ม 1");
+
+app.get("/data", async function(req, res){
+
+  try{
+
+    const url =
+      "https://sheets.googleapis.com/v4/spreadsheets/"
+      + SHEET_ID
+      + "/values/"
+      + RANGE
+      + "?key="
+      + API_KEY;
+
+    console.log("Loading data...");
+
+    const response =
+      await axios.get(url);
+
+    const rows =
+      response.data.values || [];
+
+    if(rows.length === 0){
+
+      return res.json({
+        headers: [],
+        data: []
+      });
+
+    }
+
+    const headers =
+      rows[0];
+
+    const data =
+      rows.slice(1);
+
+    console.log(
+      "Loaded " +
+      data.length +
+      " rows"
+    );
+
+    res.json({
+      headers: headers,
+      data: data
+    });
+
+  }
+
+  catch(error){
+
+    console.log("ERROR");
+
+    console.log(
+      error.response?.data
+      || error.message
+    );
+
+    res.status(500).json({
+
+      success:false,
+
+      error:
+        error.response?.data
+        || error.message
+
+    });
+
+  }
+
+});
+
+app.get("/", function(req, res){
+
+  res.sendFile(
+    __dirname + "/public/index.html"
+  );
+
+});
+
+app.listen(PORT, function(){
+
+  console.log(
+    "Server running on port "
+    + PORT
+  );
+
+});
